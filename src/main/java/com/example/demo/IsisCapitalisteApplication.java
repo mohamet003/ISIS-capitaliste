a5ccd5bcf317f4064fcb2954329fadd908754834
mohamet003 <mohamet.kone@etud.univ-jfc.fr>
2020-06-03 11:33:37 +0200

    first commit

---
+++ src/main/java/com/example/demo/IsisCapitalisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsisCapitalisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsisCapitalisteApplication.class, args);
+    }
+
+}
